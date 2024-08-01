ec39d7cf2c1bb7a27f206099f32ce385234f9e59
Shreshthangshu Chanda <shreshyboi.com@gmail.com>
2024-08-01 22:30:03 +0600

    This is a simple python project. It is game....

---
+++ CarGame.py
@@ -0,0 +1,31 @@
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True    
+            print("Car started...ready to Go!")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+             started = False              
+             print("Car stopped...")
+    elif command == "help":
+        print("""    
+start - to start the car
+stop - to stop the car
+quit - to quit
+         """ )
+    elif command == "quit":
+        break
+    else:
+        print("sorry, I don't understand the command...")   
+              
+              
+                     
+        
+    
